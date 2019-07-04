9a7783e58756ef42384c857a4f3bf16173a3a27b
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-07-04 20:02:33 +0900

    july 4th thur. 8pm

--- Crecre/app/src/main/java/com/crecrew/crecre/UI/Activity/VoteSuggestActivity.kt
+++ Crecre/app/src/main/java/com/crecrew/crecre/UI/Activity/VoteSuggestActivity.kt
@@ -4,44 +4,48 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.crecrew.crecre.R
-import kotlinx.android.synthetic.main.activity_vote_suggest.*
-
+import kotlinx.android.synthetic.main.activity_vote_suggest5.*
+var stopClick: Int = 2;
 class VoteSuggestActivity:AppCompatActivity() {
 
     var item_list = ArrayList<String>()
-    var stopClick: Int = 2;
+
 
     fun changePinkGray(v: View){
         v.setOnFocusChangeListener(){v,hasFocus->
             if (hasFocus) v.setBackgroundResource(R.drawable.vote_suggest_pink_back);
-            else v.setBackgroundResource(R.drawable.vote_suggest_additem_gray);
+            else v.setBackgroundResource(R.drawable.vote_suggest_gray_back);
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_vote_suggest)
+        setContentView(R.layout.activity_vote_suggest5)
+        vote_suggest_typing_item5.setVisibility(View.GONE);
+        vote_suggest_typing_item4.setVisibility(View.GONE);
+        vote_suggest_typing_item3.setVisibility(View.GONE);
 
         vote_suggest_additem.setOnClickListener{
-            stopClick += 1;
-            if (stopClick <= 5)
-            {
-                if (stopClick == 3){setContentView(R.layout.activity_vote_suggest3)}
-                else if (stopClick == 4){setContentView(R.layout.activity_vote_suggest4)}
-                else if (stopClick == 5){setContentView(R.layout.activity_vote_suggest5)}
-            }
-            else
-            {
-                setContentView(R.layout.activity_vote_suggest5)
-                vote_suggest_additem.setBackgroundResource(R.drawable.vote_suggest_additem_gray) //회색으로 바꿔
-            }
+            stopClick = stopClick + 1;
+
+                if (stopClick == 3){
+                    vote_suggest_typing_item3.setVisibility(View.VISIBLE);
+                }
+                else if (stopClick == 4){
+                    vote_suggest_typing_item4.setVisibility(View.VISIBLE);
+                }
+                else if (stopClick == 5){
+                    vote_suggest_typing_item5.setVisibility(View.VISIBLE);
+                    vote_suggest_additem.setBackgroundResource(R.drawable.vote_suggest_additem_gray)
+                }
+
         }
         changePinkGray(vote_suggest_typing_subj);
-
-        /*vote_suggest_typing_subj.setOnFocusChangeListener() { it, hasFocus->
-            if (hasFocus) it.setBackgroundResource(R.drawable.vote_suggest_pink_back)
-        }*/
-
+        changePinkGray(vote_suggest_typing_item1);
+        changePinkGray(vote_suggest_typing_item2);
+        changePinkGray(vote_suggest_typing_item3);
+        changePinkGray(vote_suggest_typing_item4);
+        changePinkGray(vote_suggest_typing_item5);
 
     }
 
