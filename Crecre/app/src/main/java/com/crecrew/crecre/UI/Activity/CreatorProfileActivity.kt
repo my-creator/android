357bb03bed53b8ac6fe675d5f4093be44888aba4
sungdongy <yoosd424@gmail.com>
2019-07-09 01:54:59 +0900

    profile join stat view

--- Crecre/app/src/main/java/com/crecrew/crecre/UI/Activity/CreatorProfileActivity.kt
+++ Crecre/app/src/main/java/com/crecrew/crecre/UI/Activity/CreatorProfileActivity.kt
@@ -3,7 +3,10 @@ package com.crecrew.crecre.UI.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.crecrew.crecre.R
+import com.crecrew.crecre.UI.Activity.Community.CommunityHotPostActivity
 import kotlinx.android.synthetic.main.activity_creator_profile.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.support.v4.startActivity
 
 class CreatorProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,13 @@ class CreatorProfileActivity : AppCompatActivity() {
         val intent = intent
         val creator_name = intent.getStringExtra("creator_name")
 
+        activity_creator_profile_btn_go_fanpage.setOnClickListener{
+            startActivity<CommunityHotPostActivity>()
+        }
+
+        activity_creator_profile_btn_join_stat.setOnClickListener{
+            startActivity<ProfileJoinStatActivity>()
+        }
         // activity_creator_profile_tv_name.setText(creator_name)
 
     }
