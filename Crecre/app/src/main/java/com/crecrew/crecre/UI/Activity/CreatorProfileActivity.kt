6ef4da89d808752f157ccd8c1636d386f6fd07fd
sungdongy <yoosd424@gmail.com>
2019-07-10 01:09:56 +0900

    profile video

--- Crecre/app/src/main/java/com/crecrew/crecre/UI/Activity/CreatorProfileActivity.kt
+++ Crecre/app/src/main/java/com/crecrew/crecre/UI/Activity/CreatorProfileActivity.kt
@@ -2,16 +2,29 @@ package com.crecrew.crecre.UI.Activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import com.crecrew.crecre.Data.ProfileHotVideoData
+import com.crecrew.crecre.Data.TodayPost
 import com.crecrew.crecre.R
 import com.crecrew.crecre.UI.Activity.Community.CommunityHotPostActivity
+import com.crecrew.crecre.UI.Adapter.ProfileHotVideoRecyclerViewAdapter
+import com.crecrew.crecre.UI.Adapter.TodayPostRecyclerViewAdapter
 import kotlinx.android.synthetic.main.activity_creator_profile.*
+import kotlinx.android.synthetic.main.fragment_home.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.support.v4.startActivity
+import java.util.ArrayList
 
 class CreatorProfileActivity : AppCompatActivity() {
+
+    lateinit var profileHotVideoRecyclerViewAdapter: ProfileHotVideoRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_creator_profile)
+        configureRecyclerView()
 
         val intent = intent
         val creator_name = intent.getStringExtra("creator_name")
@@ -24,6 +37,32 @@ class CreatorProfileActivity : AppCompatActivity() {
             startActivity<ProfileJoinStatActivity>()
         }
         // activity_creator_profile_tv_name.setText(creator_name)
+
+    }
+
+    private fun configureRecyclerView(){
+        var profileHotDataList: ArrayList<ProfileHotVideoData> = ArrayList()
+
+        profileHotDataList.add(ProfileHotVideoData(2, "대박대박 정호의 누네띄네 100개 먹방 영상", "https://www.naver.com", 1200000, "https://i.ytimg.com/vi/SzJo9QfhZg8/maxresdefault.jpg","2019-08-07 04:51","정호네식당"))
+        profileHotDataList.add(ProfileHotVideoData(4, "대박대박 시연의 좋은데이 100개 먹방 영상", "https://www.naver.com", 1000000,"https://i.ytimg.com/vi/SzJo9QfhZg8/maxresdefault.jpg","2019-10-07 04:51","시연네식당"))
+        profileHotDataList.add(ProfileHotVideoData(6, "대박대박 홍삼의 고구마간식 100개 먹방 영상", "https://www.naver.com", 120000, "https://i.ytimg.com/vi/SzJo9QfhZg8/maxresdefault.jpg","2019-07-02 04:51","홍삼네식당"))
+
+        profileHotVideoRecyclerViewAdapter = ProfileHotVideoRecyclerViewAdapter(this, profileHotDataList)
+        activity_creator_profile_rv_hot_video.adapter = profileHotVideoRecyclerViewAdapter
+        activity_creator_profile_rv_hot_video.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        activity_creator_profile_rv_hot_video.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+
+        // new post
+        var profileNewDataList: ArrayList<ProfileHotVideoData> = ArrayList()
+
+        profileNewDataList.add(ProfileHotVideoData(2, "대박대박 정호의 누네띄네 100개 먹방 영상", "https://www.naver.com", 1200000, "https://i.ytimg.com/vi/SzJo9QfhZg8/maxresdefault.jpg","2019-07-07 04:51","정호네식당"))
+        profileNewDataList.add(ProfileHotVideoData(4, "대박대박 시연의 좋은데이 100개 먹방 영상", "https://www.naver.com", 1000000,"https://i.ytimg.com/vi/SzJo9QfhZg8/maxresdefault.jpg","2019-07-03 04:51","시연네식당"))
+        profileNewDataList.add(ProfileHotVideoData(6, "대박대박 홍삼의 고구마간식 100개 먹방 영상", "https://www.naver.com", 120000,"https://i.ytimg.com/vi/SzJo9QfhZg8/maxresdefault.jpg","2019-07-07 12:51","홍삼네식당"))
+
+        profileHotVideoRecyclerViewAdapter = ProfileHotVideoRecyclerViewAdapter(this, profileNewDataList)
+        activity_creator_profile_rv_new_video.adapter = profileHotVideoRecyclerViewAdapter
+        activity_creator_profile_rv_new_video.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        activity_creator_profile_rv_new_video.addItemDecoration(DividerItemDecoration(this!!, DividerItemDecoration.VERTICAL))
 
     }
 
---
+++ Crecre/app/src/main/java/com/crecrew/crecre/UI/Adapter/ProfileHotVideoRecyclerViewAdapter.kt
@@ -0,0 +1,86 @@
+package com.crecrew.crecre.UI.Adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.crecrew.crecre.Data.ProfileHotVideoData
+import com.crecrew.crecre.Data.TodayPost
+import com.crecrew.crecre.R
+import com.crecrew.crecre.UI.Activity.Community.CommunityDetailActivity
+import org.jetbrains.anko.startActivity
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class ProfileHotVideoRecyclerViewAdapter(private val ctx : Context, private val dataList : ArrayList<ProfileHotVideoData>) : RecyclerView.Adapter<ProfileHotVideoRecyclerViewAdapter.Holder>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): Holder {
+        val view: View = LayoutInflater.from(ctx)!!.inflate(R.layout.rv_item_today_post, viewGroup, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+
+
+        if (dataList[position].thumbnail_url == "")
+            Glide.with(ctx).load(R.drawable.icn_img_x).into(holder.thumbnail_url)
+        else
+            Glide.with(ctx).load(dataList[position].thumbnail_url).into(holder.thumbnail_url)
+        holder.title.text = dataList[position].title
+        holder.view_cnt.text = "조회수" + dataList[position].view_cnt.toString()
+        holder.create_time.text = calculatePostTime(dataList[position].create_time.toString())
+
+        holder.container.setOnClickListener {
+            ctx.startActivity<CommunityDetailActivity>(
+                // TODO: 정보 넣기
+            )
+        }
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var container = itemView.findViewById(R.id.rv_item_today_post_container) as RelativeLayout
+        var thumbnail_url = itemView.findViewById(R.id.rv_item_today_post_thumnail) as ImageView
+        var title = itemView.findViewById(R.id.rv_item_today_post_title) as TextView
+        var create_time = itemView.findViewById(R.id.rv_item_today_post_recommend) as TextView
+        var view_cnt = itemView.findViewById(R.id.rv_item_today_post_category) as TextView
+
+    }
+
+    fun calculatePostTime(time : String) : String{
+
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm")
+        val write_time : Date =  sdf.parse(time)
+        val cur_time = Date()
+
+        val diff : Long = cur_time.time - write_time.time
+        var minute = diff / (1000 * 60)
+
+        var result : String
+        if(minute >= 0 && minute < 60)
+            result = "$minute" + "분 전"
+        else if(minute >= 60 && minute < 60*24) {
+            minute /= 60
+            result = "$minute" + "시간 전"
+        }
+        else if(minute >=60*24 && minute <60*24*30){
+            minute /= (60 * 24)
+            result = "$minute" + "일 전"
+        }
+        else if(minute >=60*24*30 && minute <60*24*365){
+            minute /= (60 * 24*30)
+            result = "$minute" + "달 전"
+        }else{
+            minute /= (60*24*365)
+            result = "$minute" + "년 전"
+        }
+        return result
+    }
+}
