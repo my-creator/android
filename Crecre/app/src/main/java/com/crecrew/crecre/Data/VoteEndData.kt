f89827b3cb77abf6f25705dd318ac95012878848
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-07-09 18:15:56 +0900

    Merge branch 'develope' of https://github.com/my-creator/my-creator-android
    # Conflicts:
    #	Crecre/app/src/main/res/layout/vote_card.xml
    #votecurrentItemadapter, votecurrentadapter

---
+++ Crecre/app/src/main/java/com/crecrew/crecre/Data/VoteEndData.kt
@@ -0,0 +1,10 @@
+package com.crecrew.crecre.Data
+
+data class VoteEndData (
+    var ImageURL: String,
+    var title: String,
+    var explain: String,
+    var itemname1: String, var itemname2: String, var itemname3: String, var itemname4: String, var itemname5: String,
+    var itemimage1: String, var itemimage2: String, var itemimage3: String, var itemimage4: String, var itemimage5: String,
+    var votenum1: Int, var votenum2: Int, var votenum3: Int, var votenum4: Int, var votenum5: Int
+    )
