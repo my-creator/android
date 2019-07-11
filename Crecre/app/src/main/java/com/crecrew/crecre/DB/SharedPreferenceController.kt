570ae0be1b9e62b77d1cb8e03184bee4cc2ba928
Yang Si Yeon <mn04098@naver.com>
2019-07-12 04:53:33 +0900

    implement signup, signin feature

---
+++ Crecre/app/src/main/java/com/crecrew/crecre/DB/SharedPreferenceController.kt
@@ -0,0 +1,29 @@
+package com.crecrew.crecre.DB
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.crecrew.crecre.Network.Post.Token
+
+object SharedPreferenceController {
+
+    val MY_ACCOUNT = "unique_string"
+
+    fun setUserToken(ctx: Context, u_token: String?) {
+        val preference: SharedPreferences = ctx.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = preference.edit()
+        editor.putString("token", u_token)
+        editor.commit()
+    }
+
+    fun getUserToken(ctx: Context): String {
+        val preference: SharedPreferences = ctx.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        return preference.getString("u_id", "")
+    }
+
+    fun clearUserToken(ctx: Context) {
+        val preference: SharedPreferences = ctx.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = preference.edit()
+        editor.clear()
+        editor.commit()
+    }
+}
--- Crecre/app/src/main/java/com/crecrew/crecre/Network/UserNetworkService.kt
+++ Crecre/app/src/main/java/com/crecrew/crecre/Network/UserNetworkService.kt
@@ -2,15 +2,29 @@ package com.crecrew.crecre.Network
 
 import com.crecrew.crecre.Network.Get.GetIdDuplicateResponse
 import com.crecrew.crecre.Network.Get.GetRankResponse
+import com.crecrew.crecre.Network.Post.PostLoginResponse
+import com.crecrew.crecre.Network.Post.PostSignupResponse
+import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface UserNetworkService {
 
     @GET("auth/duplicate/{id}")
     fun getIdDuplicate(
-        @Path("id") id:String
+        @Path("id") id: String
     ): Call<GetIdDuplicateResponse>
+
+    @POST("auth/signup")
+    fun postSignup(
+        @Body() body: JsonObject
+    ): Call<PostSignupResponse>
+
+    @POST("auth/signin")
+    fun postLogin(
+        @Body() body: JsonObject
+    ): Call<PostLoginResponse>
 }
