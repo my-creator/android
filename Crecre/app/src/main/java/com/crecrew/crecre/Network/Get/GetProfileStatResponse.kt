697ce7062e0999d40506c0cae60571cb29e4d65c
sungdongy <yoosd424@gmail.com>
2019-07-13 01:52:41 +0900

    GOD MOONJUNGHO

--- Crecre/app/src/main/java/com/crecrew/crecre/Network/Get/GetProfileStatResponse.kt
+++ Crecre/app/src/main/java/com/crecrew/crecre/Network/Get/GetProfileStatResponse.kt
@@ -6,5 +6,17 @@ data class GetProfileStatResponse (
     val status : Int,
     val success : Boolean,
     val message : String,
-    val data : ArrayList<StatData>
+    val data : StatDataTest
+)
+
+data class StatDataTest (
+    val avg_stat: Double,
+    val join_cnt_stat: Int,
+    val stat : ArrayList<StatDataTest2>
+)
+
+data class StatDataTest2 (
+    val name: String,
+    val stat_idx: Int,
+    val stat_score: Float
 )
--- Crecre/app/src/main/java/com/crecrew/crecre/UI/Activity/CreatorProfileActivity.kt
+++ Crecre/app/src/main/java/com/crecrew/crecre/UI/Activity/CreatorProfileActivity.kt
@@ -4,27 +4,20 @@ import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
-import android.support.v7.widget.DividerItemDecoration
-import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.RelativeLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.crecrew.crecre.Base.BasePagerAdapter
 import com.crecrew.crecre.Data.*
 import com.crecrew.crecre.Network.ApplicationController
 import com.crecrew.crecre.Network.CreatorNetworkService
-import com.crecrew.crecre.Network.Get.GetCreatorTodayHotRank
-import com.crecrew.crecre.Network.Get.GetProfileHotVideoResponse
 import com.crecrew.crecre.Network.Get.GetProfileResponse
 import com.crecrew.crecre.Network.Get.GetProfileStatResponse
+import com.crecrew.crecre.Network.Get.StatDataTest
+import com.crecrew.crecre.Network.Get.StatDataTest2
 import com.crecrew.crecre.R
 import com.crecrew.crecre.UI.Activity.Community.CommunityHotPostActivity
 import com.crecrew.crecre.UI.Adapter.ProfileHotVideoRecyclerViewAdapter
-import com.crecrew.crecre.UI.Fragment.HomeTodayRankFragment
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.RadarChart
 import com.github.mikephil.charting.components.AxisBase
@@ -49,13 +42,25 @@ class CreatorProfileActivity : FragmentActivity() {
     lateinit var mChart: RadarChart
 
     //"진행능력", "소통력", "참신성", "편집력", "핫 지수"
-    // val testItem = arrayListOf<String>("진행능력", "소통력", "참신성", "편집력", "핫 지수")
+    val testItem = arrayListOf<String>()
+    val entries1 = ArrayList<RadarEntry>()
 
-    lateinit var creatorProfileData : CreatorProfileData
-    lateinit var statData: StatData
+    lateinit var creatorProfileData: CreatorProfileData //크리에이터 정보
+    lateinit var statData: StatDataTest //xAxis
     lateinit var mTypeface: Typeface
+    val statDataTest = StatDataTest(
+        4.8,
+        32,
+        arrayListOf(
+            StatDataTest2("진행능력", 0, 4.8.toFloat()),
+            StatDataTest2("소통력", 0, 4.8.toFloat()),
+            StatDataTest2("참신성", 0, 4.8.toFloat()),
+            StatDataTest2("편집력", 0, 4.8.toFloat()),
+            StatDataTest2("핫 지수", 0, 4.8.toFloat())
+        )
+    )
 
-    val creatorNetworkService: CreatorNetworkService by lazy{
+    val creatorNetworkService: CreatorNetworkService by lazy {
         ApplicationController.instance.creatorNetworkService
     }
 
@@ -66,10 +71,22 @@ class CreatorProfileActivity : FragmentActivity() {
 
         mTypeface = activity_creator_profile_review_number.typeface
 
-        
-        getProfileResponse(creator_idx)
-        getProfileStatResponse(creator_idx)
+        var creator_idx = 5123
 
+        var intent = intent.getIntExtra("creator_idx", 5123)
+        getProfileResponse(intent)
+
+        statDataTest.stat?.let {
+            for (i in it.indices)
+                testItem.add(it[i].name)
+            for (i in it.indices)
+                entries1.add(RadarEntry(it[i].stat_score))
+        }
+//        getProfileStatResponse(intent)
+
+        makeGraph(testItem, entries1)
+        activity_creator_profile_total.text = "토탈 " + String.format("%.2f", statDataTest.avg_stat) + "점"
+        activity_creator_profile_review_number.text = "(" + statDataTest.join_cnt_stat.toString() + "명 리뷰)"
 
         activity_creator_profile_btn_go_fanpage.setOnClickListener {
             startActivity<CommunityHotPostActivity>()
@@ -93,7 +110,7 @@ class CreatorProfileActivity : FragmentActivity() {
 
     }
 
-    fun makeGraph(testItem : ArrayList<String>){
+    fun makeGraph(testItem: ArrayList<String>, entries1: ArrayList<RadarEntry>) {
 
         mChart = activity_creator_profile_rc_chart
         mChart.run {
@@ -108,16 +125,16 @@ class CreatorProfileActivity : FragmentActivity() {
             //webAlpha = 100
         }
 
-        val cnt = 5
+//        val cnt = 5
 
-        val entries1 = ArrayList<RadarEntry>()
+//        val entries1 = ArrayList<RadarEntry>()
 
         // NOTE: The order of the entries when being added to the entries array determines their position around the center of
         // the chart.
-        for (i in 0 until cnt) {
-            val val1 = (i + 1).toFloat()
-            entries1.add(RadarEntry(val1))
-        }
+//        for (i in 0 until cnt) {
+//            val val1 = (i + 1).toFloat()
+//            entries1.add(RadarEntry(val1))
+//        }
 
         val set1 = RadarDataSet(entries1, "크리에이터 스텟")
         set1.color = Color.rgb(28, 28, 28)
@@ -159,7 +176,8 @@ class CreatorProfileActivity : FragmentActivity() {
                         "${testItem[1]}\n${entries1[1].value}점",
                         "${testItem[2]}\n${entries1[2].value}점",
                         "${testItem[3]}\n${entries1[3].value}점",
-                        "${testItem[4]}\n${entries1[4].value}점")
+                        "${testItem[4]}\n${entries1[4].value}점"
+                    )
 
                     override fun getFormattedValue(value: Float, axis: AxisBase): String {
                         return mActivities[value.toInt() % mActivities.size]
@@ -179,36 +197,51 @@ class CreatorProfileActivity : FragmentActivity() {
     }
 
 
-    private fun getProfileResponse(creatorIdx: Int){
+    private fun getProfileResponse(creatorIdx: Int) {
         val getProfileResponse = creatorNetworkService.getProfileResponse(creatorIdx)
         getProfileResponse.enqueue(object : Callback<GetProfileResponse> {
             override fun onFailure(call: Call<GetProfileResponse>, t: Throwable) {
-                Log.e("creator search fail",t.toString())
+                Log.e("creator search fail", t.toString())
             }
+
             override fun onResponse(call: Call<GetProfileResponse>, response: Response<GetProfileResponse>) {
-                if(response.isSuccessful){
-                    if(response.body()!!.status == 200){
+                if (response.isSuccessful) {
+                    if (response.body()!!.status == 200) {
                         creatorProfileData = response.body()!!.data
                         Log.v("TAGG", creatorProfileData.profile_url)
-                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.profile_url).apply(RequestOptions().circleCrop()).into(profile_creator_main_img)
-                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.profile_asset).into(profile_creator_all_class)
-                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.follower_grade_img_url).into(profile_creator_rank)
+                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.profile_url)
+                            .apply(RequestOptions().circleCrop()).into(profile_creator_main_img)
+                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.profile_asset)
+                            .into(profile_creator_all_class)
+                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.follower_grade_img_url)
+                            .into(profile_creator_rank)
                         profile_creator_channel.text = creatorProfileData.creator_name
-                        creator_profile_category.text = creatorProfileData.category_name + " " + creatorProfileData.category_lank + "위"
+                        creator_profile_category.text =
+                            creatorProfileData.category_name + " " + creatorProfileData.category_lank + "위"
                         profile_follower_cnt.text = creatorProfileData.follower_cnt.toString() + "명"
-                        youtube_subscriber_cnt.text = String.format("%,d",creatorProfileData.youtube_subscriber_cnt) + "명"
-                        youtube_view_cnt.text = String.format("%,d",creatorProfileData.youtube_view_cnt) + "명"
+                        youtube_subscriber_cnt.text =
+                            String.format("%,d", creatorProfileData.youtube_subscriber_cnt) + "명"
+                        youtube_view_cnt.text = String.format("%,d", creatorProfileData.youtube_view_cnt) + "명"
                         activity_creator_profile_description.text = creatorProfileData.contents
-                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.follower_grade_img_url).into(activity_creator_profile_rank_img)
-                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.view_grade_img_url).into(activity_creator_profile_class_img)
-                        activity_creator_profile_rank_tier.text = creatorProfileData.follower_grade_name + " " + creatorProfileData.follower_grade_level
-                        activity_creator_profile_rank_percent.text = creatorProfileData.follower_grade_percent.toString() + "%"
-                        activity_creator_profile_front_rank1_exp.text = "(" + creatorProfileData.front_lank_exp.toString()
-                        activity_creator_profile_back_rank1_exp.text = "/" + creatorProfileData.back_lank_exp.toString() + ")"
+                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.follower_grade_img_url)
+                            .into(activity_creator_profile_rank_img)
+                        Glide.with(this@CreatorProfileActivity).load(creatorProfileData.view_grade_img_url)
+                            .into(activity_creator_profile_class_img)
+                        activity_creator_profile_rank_tier.text =
+                            creatorProfileData.follower_grade_name + " " + creatorProfileData.follower_grade_level
+                        activity_creator_profile_rank_percent.text =
+                            creatorProfileData.follower_grade_percent.toString() + "%"
+                        activity_creator_profile_front_rank1_exp.text =
+                            "(" + creatorProfileData.front_lank_exp.toString()
+                        activity_creator_profile_back_rank1_exp.text =
+                            "/" + creatorProfileData.back_lank_exp.toString() + ")"
                         activity_creator_profile_class_tier.text = "클래스 " + creatorProfileData.view_grade_name
-                        activity_creator_profile_class_percentage.text = creatorProfileData.view_grade_percent.toString() + "%"
-                        activity_creator_profile_front_rank2_exp.text = "(" + creatorProfileData.front_lank2_exp.toString()
-                        activity_creator_profile_back_rank2_exp.text = "/" + creatorProfileData.back_lank2_exp.toString() + ")"
+                        activity_creator_profile_class_percentage.text =
+                            creatorProfileData.view_grade_percent.toString() + "%"
+                        activity_creator_profile_front_rank2_exp.text =
+                            "(" + creatorProfileData.front_lank2_exp.toString()
+                        activity_creator_profile_back_rank2_exp.text =
+                            "/" + creatorProfileData.back_lank2_exp.toString() + ")"
                         circularProgressbar1.progress = creatorProfileData.follower_grade_percent
                         circularProgressbar2.progress = creatorProfileData.view_grade_percent
                     }
@@ -216,65 +249,71 @@ class CreatorProfileActivity : FragmentActivity() {
             }
         })
 
-        }
+    }
 
 
-    private fun getProfileStatResponse(creatorIdx: Int){
+    private fun getProfileStatResponse(creatorIdx: Int) {
         val getProfileStatResponse = creatorNetworkService.getProfileStatResponse(creatorIdx)
         getProfileStatResponse.enqueue(object : Callback<GetProfileStatResponse> {
-            override fun onFailure(call : Call<GetProfileStatResponse>, t: Throwable) {
+            override fun onFailure(call: Call<GetProfileStatResponse>, t: Throwable) {
                 Log.e("creator search fail", t.toString())
             }
-            override fun onResponse(call: Call<GetProfileStatResponse>, response: Response<GetProfileStatResponse>){
-                if(response.isSuccessful){
-                    if(response.body()!!.status == 200){
-                        // Log.e("dfdfdf", "dfdfdf")
-                        var creatorStatData: ArrayList<StatData> = response.body()!!.data
-                        // Log.e("success", "success")
-                        activity_creator_profile_total.text = "토탈 " + String.format("%.2f", creatorStatData[5].avg_stat) + "점"
-                        activity_creator_profile_review_number.text = "(" + creatorStatData[5].join_cnt_stat.toString() + "명 리뷰)"
-                        val testItem = arrayListOf(creatorStatData[0].name, creatorStatData[1].name, creatorStatData[2].name, creatorStatData[3].name, creatorStatData[4].name)
-                        makeGraph(testItem)
-                    }
-                }
-            }
-        })
-    }
-    private fun getProfileHotVideo(creatorIdx: Int){
-        val getProfileHotVideResponse = creatorNetworkService.getProfileHotVideoResponse(creatorIdx)
-        getProfileHotVideResponse.enqueue(object : Callback<GetProfileHotVideoResponse> {
-            override fun onFailure(call: Call<GetProfileHotVideoResponse>, t: Throwable) {
-                Log.e("creator search fail", t.toString())
-            }
-            override fun onResponse(call: Call<GetProfileHotVideoResponse>, response: Response<GetProfileHotVideoResponse>){
-                if(response.isSuccessful){
-                    if(response.body()!!.status == 200){
-                        var HotVideoDatas : ArrayList<HotVideoData> = response.body()!!.data
-                        configureRecyclerView(HotVideoDatas)
+
+            override fun onResponse(call: Call<GetProfileStatResponse>, response: Response<GetProfileStatResponse>) {
+                if (response.isSuccessful) {
+                    if (response.body()!!.status == 200) {
+                        statData = response.body()!!.data
+                        activity_creator_profile_total.text = "토탈 " + String.format("%.2f", statData.avg_stat) + "점"
+                        activity_creator_profile_review_number.text = "(" + statData.join_cnt_stat.toString() + "명 리뷰)"
+                        statData.stat?.let {
+                            for (i in it.indices)
+                                testItem.add(it[i].name)
+                            for (i in it.indices)
+                                entries1.add(RadarEntry(it[i].stat_score))
+                        }
+//                        makeGraph(testItem, entries1)
                     }
                 }
             }
         })
     }
 
-    private fun configureRecyclerView(profileHotDataList : ArrayList<HotVideoData>){
 
+//    private fun getProfileHotVideo(creatorIdx: Int){
+//        val getProfileHotVideResponse = creatorNetworkService.getProfileHotVideoResponse(creatorIdx)
+//        getProfileHotVideResponse.enqueue(object : Callback<GetProfileHotVideoResponse> {
+//            override fun onFailure(call: Call<GetProfileHotVideoResponse>, t: Throwable) {
+//                Log.e("creator search fail", t.toString())
+//            }
+//            override fun onResponse(call: Call<GetProfileHotVideoResponse>, response: Response<GetProfileHotVideoResponse>){
+//                if(response.isSuccessful){
+//                    if(response.body()!!.status == 200){
+//                        var HotVideoDatas : ArrayList<HotVideoData> = response.body()!!.data
+//                        // configureRecyclerView(HotVideoDatas)
+//                    }
+//                }
+//            }
+//        })
+//    }
 
-        // dfsljfsdajasdfjla;njajananagnanavnavnoananarvn
-        profileHotVideoRecyclerViewAdapter = ProfileHotVideoRecyclerViewAdapter(this, profileHotDataList)
-        activity_creator_profile_rv_hot_video.adapter = profileHotVideoRecyclerViewAdapter
-        activity_creator_profile_rv_hot_video.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        activity_creator_profile_rv_hot_video.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
-
-        // new post
+//    private fun configureRecyclerView(){
+//
+//
+//        var profileHotDataList : ArrayList<ProfileHotVideoData> = ArrayList()
+//        profileHotVideoRecyclerViewAdapter = ProfileHotVideoRecyclerViewAdapter(this, profileHotDataList)
+//        activity_creator_profile_rv_hot_video.adapter = profileHotVideoRecyclerViewAdapter
+//        activity_creator_profile_rv_hot_video.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+//        activity_creator_profile_rv_hot_video.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+//
+//        // new post
 //        var profileNewDataList: ArrayList<ProfileHotVideoData> = ArrayList()
 //
 //        profileHotVideoRecyclerViewAdapter = ProfileHotVideoRecyclerViewAdapter(this, profileNewDataList)
 //        activity_creator_profile_rv_new_video.adapter = profileHotVideoRecyclerViewAdapter
 //        activity_creator_profile_rv_new_video.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 //        activity_creator_profile_rv_new_video.addItemDecoration(DividerItemDecoration(this!!, DividerItemDecoration.VERTICAL))
-
-    }
+//
+//    }
 
     private fun dataSetAvg(dataSet: ArrayList<RadarEntry>): Float {
         var total = 0f
@@ -282,8 +321,6 @@ class CreatorProfileActivity : FragmentActivity() {
             total += dataSet[i].value
         return total / dataSet.size
     }
-
-
 
 
 }
